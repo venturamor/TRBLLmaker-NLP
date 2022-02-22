a1e2cd665c3709518598fad66216604cd118b128
Michael Toker <dontokeron@gmail.com>
2022-02-22 23:55:11 +0200

    #5
    Fix bug in set_config
    Updated parameters that the server can handle for now

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #	arguments: 											dataset_name	 					model_name				num_train_epochs		batch_size		learning_rate
 echo 'Starting set_config...'		                               			
-python set_config.py 							TRBLL_dataset_mini.py		t5-small					1										8							0.0000001
-echo 'Starting first run...'		 
-python run.py
+python set_config.py 							TRBLL_dataset_mini.py		t5-small						4										16						0.000001
+echo 'Starting first run...'
+python T5_vanilla_model.py
 echo 'Finished first run...'
-python set_config.py 							TRBLL_dataset.py				t5-base						5										16						0.0000001
-python run.py
-python set_config.py 	  					TRBLL_dataset.py				t5-base						5										16						0.0000001
-python run.py
-python set_config.py 							TRBLL_dataset.py				t5-base						5										16						0.0000001
-python run.py
-python set_config.py	  					TRBLL_dataset.py				t5-base						5										32						0.0000001
-python run.py
+python set_config.py 							TRBLL_dataset.py		    t5-small						4										16						0.000001
+python T5_vanilla_model.py
+python set_config.py 							TRBLL_dataset.py				t5-small						8										16						0.0000008
+python T5_vanilla_model.py
+python set_config.py 	  					TRBLL_dataset.py				t5-small						16									16						0.0000004
+python T5_vanilla_model.py
+python set_config.py 							TRBLL_dataset.py				t5-small						32									16						0.0000002
+python T5_vanilla_model.py
+python set_config.py	  					TRBLL_dataset.py				t5-small						5										16						0.0000001
+python T5_vanilla_model.py
 echo 'Finished...'
-
-
--- set_config.py
+++ set_config.py
@@ -4,10 +4,10 @@ import sys
 f = open("config.yaml")
 y = yaml.safe_load(f)
 y["train_args"]["dataset_name"] = sys.argv[1]
-y["model_args"]["model_name"] = float(sys.argv[2])
-y["model_args"]["num_train_epochs"] = int(sys.argv[3])
-y["model_args"]["batch_size"] = int(sys.argv[4])
-y["model_args"]["learning_rate"] = float(sys.argv[5])
+y["train_args"]["model_name"] = sys.argv[2]
+y["train_args"]["num_train_epochs"] = int(sys.argv[3])
+y["train_args"]["batch_size"] = int(sys.argv[4])
+y["train_args"]["learning_rate"] = float(sys.argv[5])
 f = open("config.yaml", "w")
 yaml.dump(y, f)
 f.close()
