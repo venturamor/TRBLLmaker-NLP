b48537b052b2b4d690ada88d440535a2dd54f792
Mor Ventura <venturamor78@gmail.com>
2022-02-24 11:57:55 +0200

    config back to T5_vanilla_model.py (#5) (#2)

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#	arguments: 											dataset_name	 					take_mini     model_name				num_train_epochs		batch_size		learning_rate
+#	arguments: 											dataset_name	 				     model_name				num_train_epochs		batch_size		learning_rate
 echo 'Starting set_config...'		                               			
-python set_config.py 							TRBLL_dataset.py		     0.1          t5-small						4										16						0.000001
+python set_config.py 							TRBLL_dataset.py		          t5-small						4										16						0.000001
 echo 'Starting first run...'
 python T5_vanilla_model.py
 echo 'Finished first run...'
-python set_config.py 							TRBLL_dataset.py		     0            t5-small						4										16						0.000001
+python set_config.py 							TRBLL_dataset.py		          t5-small						4										16						0.000001
 python T5_vanilla_model.py
-python set_config.py 							TRBLL_dataset.py				 0            t5-small						8										16						0.0000008
+python set_config.py 							TRBLL_dataset.py				      t5-small						8										16						0.0000008
 python T5_vanilla_model.py
-python set_config.py 	  					TRBLL_dataset.py				 0            t5-small						16									16						0.0000004
+python set_config.py 	  					TRBLL_dataset.py				      t5-small						16									16						0.0000004
 python T5_vanilla_model.py
-python set_config.py 							TRBLL_dataset.py				 0            t5-small						32									16						0.0000002
+python set_config.py 							TRBLL_dataset.py				      t5-small						32									16						0.0000002
 python T5_vanilla_model.py
-python set_config.py	  					TRBLL_dataset.py				 0            t5-small						5										16						0.0000001
+python set_config.py	  					TRBLL_dataset.py				      t5-small						5										16						0.0000001
 python T5_vanilla_model.py
 echo 'Finished...'
