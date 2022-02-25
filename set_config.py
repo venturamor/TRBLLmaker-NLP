78ad88c41107f4af9f180d76e399228f928be7a6
Michael Toker <dontokeron@gmail.com>
2022-02-25 20:11:11 +0200

    #6
    Generic set_config

--- set_config.py
+++ set_config.py
@@ -3,12 +3,16 @@ import sys
 
 f = open("config.yaml")
 y = yaml.safe_load(f)
-y["train_args"]["dataset_name"] = sys.argv[1]
-# y["train_args"]["take_mini"] = sys.argv[2]
-y["train_args"]["model_name"] = sys.argv[2]
-y["train_args"]["num_train_epochs"] = int(sys.argv[3])
-y["train_args"]["batch_size"] = int(sys.argv[4])
-y["train_args"]["learning_rate"] = float(sys.argv[5])
+for i in range(1, len(sys.argv)):
+    var, val = sys.argv[i].split("=")
+    if var.find("_int") != -1:
+        var = var.split('_int')[0]
+        y["train_args"][var] = int(val)
+    elif var.find("_float") != -1:
+        var = var.split('_float')[0]
+        y["train_args"][var] = float(val)
+    else:
+        y["train_args"][var] = val
 f = open("config.yaml", "w")
 yaml.dump(y, f)
 f.close()
